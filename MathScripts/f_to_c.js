b9b6885a4c3fe0bb3058bcdab0eea6b631294104
VVSteven <21elvisc@gmail.com>
2023-10-18 15:26:49 -0700

    created ifscripts folder and pay rules js file

--- MathScripts/f_to_c.js
+++ MathScripts/f_to_c.js
@@ -1,16 +1,22 @@
 //  Fahrenheit to Celsius 
 // formula to convert fahrenheit to celsius; (F-32) 5/9 
 
+//takes away 32 from fahrenheit
 let minuse32 = 100 - 32;
 
+//outputs away 32 from fahrenheit
 console.log( minuse32)
 
+//multiplies previous answer by 5
 let times5 = minuse32 * 5;
 
+//outputs answer
 console.log( times5)
 
+//divides previous answer by 9
 let divide9 = times5 / 9 ;
 
+//outputs answer
 console.log( divide9.toFixed(2))
 
 //Celcius to Fahrenheit
---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,20 @@
+// Description: This script tests various numeric
+// conversion techniques
+// Author: Elvis Canastuj 
+
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+aParseInt = parseInt(a)
+console.log (aParseInt)
+
+bParseInt = parseInt(b)
+console.log (bParseInt)
+
+cParseInt = parseInt(c)
+console.log (cParseInt)
+
+dParseInt = parseInt(d)
+console.log (dParseInt)
