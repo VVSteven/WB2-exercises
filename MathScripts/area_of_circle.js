bab24cdec60bc7f41ff14339cfc3ddb444d3419f
VVSteven <21elvisc@gmail.com>
2023-10-18 13:17:16 -0700

    made a area of circle calculater

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,14 @@
+//calculates area of a circle
+// formula for calculating the area of a circle is: pi * radius^2 = area
+
+//radius of circle
+let radiusOfCircle = 7;
+//pi
+let pi = Math.PI;
+
+//the equation to get the area of the circle 
+const areaOfCircle = pi * radiusOfCircle * radiusOfCircle;
+
+//outputs the area of the circle
+console.log("The area of a circle with radius some-number is: "+ areaOfCircle.toFixed(2));
+
--- MathScripts/tip_amount.js
+++ MathScripts/tip_amount.js
@@ -1,12 +1,18 @@
 //total tip amount
 // formaula to calculate tip amount is: 
 
+// total of the food 
 let foodTotal = 230.45;
-let tipTotal = .20 * foodTotal;
+// 20% tip of the food
+let tipTotal = .20 * foodTotal; 
 
+// shows output of food bill
 console.log("total food bill : " + foodTotal);
+// shows output of tip
 console.log("total tip       : " + tipTotal);
 
+// calculates the total of price of the food and tip
 const totalPrice = foodTotal + tipTotal;
 
+//outputs the total price of the food and the tip
 console.log("Total           : " + totalPrice.toFixed(2));
