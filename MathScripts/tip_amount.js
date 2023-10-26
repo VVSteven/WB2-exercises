d0258c07e713e3c17672b9d928dd2ae272754fe1
VVSteven <21elvisc@gmail.com>
2023-10-26 04:45:40 -0700

    updated area of tip amount js

--- MathScripts/tip_amount.js
+++ MathScripts/tip_amount.js
@@ -2,17 +2,11 @@
 // formaula to calculate tip amount is: 
 
 // total of the food 
-let foodTotal = 230.45;
+const foodTotal = 230.45;
 // 20% tip of the food
-let tipTotal = .20 * foodTotal; 
-
-// shows output of food bill
-console.log("total food bill : " + foodTotal);
-// shows output of tip
-console.log("total tip       : " + tipTotal);
-
+const tipTotal = .20 * foodTotal; 
 // calculates the total of price of the food and tip
 const totalPrice = foodTotal + tipTotal;
 
 //outputs the total price of the food and the tip
-console.log("Total           : " + totalPrice.toFixed(2));
+console.log("The tip on a " + totalPrice.toFixed(2) + " food bill is " + tipTotal);
