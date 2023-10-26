8e9d9941e4b74d4ebdc62bf9fac53cf00a1e8b2a
VVSteven <21elvisc@gmail.com>
2023-10-26 07:13:18 -0700

    completed distance js

--- MathScripts/distance.js
+++ MathScripts/distance.js
@@ -2,11 +2,16 @@
 // fomula to calculate distance is: d=√'(x2-x1)^2+(y2-y1)^2'
 
 //point 1 (5,10) 
-let x1 = 5
-let y1 = 10
+const xOne = 5
+const yOne = 10
 
 //point 2(20,15)
-let x2 = 20
-let y2 = 15 
+const xTwo = 20
+const yTwo = 15 
+
+let distance = Math.sqrt(Math.pow((xTwo - xOne),2) + Math.pow((yTwo - yOne),2));
+console.log(distance);
+
+
 
 
