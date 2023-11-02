a90d74bdf7120fe0eada821168c201be8ba9cdb5
VVSteven <21elvisc@gmail.com>
2023-11-01 20:49:44 -0700

    completed converstion test js

--- NumericConversions/conversion_tests.js
+++ NumericConversions/conversion_tests.js
@@ -7,14 +7,56 @@ var b = "55";
 var c = "402 Stevens";
 var d = "Number 5 ";
 
-aParseInt = parseInt(a)
-console.log (aParseInt)
+aParseInt = parseInt(a);
+console.log(aParseInt)
 
-bParseInt = parseInt(b)
-console.log (bParseInt)
+aParseFloat = parseFloat(a);
+console.log(aParseFloat)
 
-cParseInt = parseInt(c)
-console.log (cParseInt)
+aNumber = Number(a);
+console.log(aNumber)
 
-dParseInt = parseInt(d)
-console.log (dParseInt)
+aUnary = +a;
+console.log(aUnary)
+
+console.log("----------------------------------------------------")
+
+bParseInt = parseInt(b);
+console.log(bParseInt)
+
+bParseFloat = parseFloat(b);
+console.log(bParseFloat)
+
+bNumber = Number(b);
+console.log(bNumber)
+
+bUnary = +b;
+console.log(bUnary)
+
+console.log("----------------------------------------------------")
+
+cParseInt = parseInt(c);
+console.log(cParseInt)
+
+cParseFloat = parseFloat(c);
+console.log(cParseFloat)
+
+cNumber = Number(c);
+console.log(cNumber)
+
+cUnary = +c;
+console.log(cUnary)
+
+console.log("----------------------------------------------------")
+
+dParseInt = parseInt(d);
+console.log(dParseInt)
+
+dParseFloat = parseFloat(d);
+console.log(dParseFloat)
+
+dNumber = Number(d);
+console.log(dNumber)
+
+dUnary = +d;
+console.log(dUnary)
