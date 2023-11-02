8fd337c757067de0343dbabe025c80379998bd04
VVSteven <21elvisc@gmail.com>
2023-11-01 22:14:03 -0700

    completed the greeting js

---
+++ IfScripts/greeting.js
@@ -0,0 +1,12 @@
+
+let time = 23;
+
+if (time >= 0 && time < 10) {
+    console.log("Good Morning!");
+
+} else if (time >= 10 && time < 17) {
+    console.log("Good Day!");
+
+} else if (time >= 17 && time < 24) {
+    console.log("Good Evening!");
+}
